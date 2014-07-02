c919cb01a777fcf3bbb0d83d8ea2c6b99e20e368
Anonoz Chong <everything@anonoz.com>
2014-07-02 20:58:20 +0800

    Redirect if no mysgdev.com

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,5 @@
-console.log('This would be the main JS file.');
+<script>
+    if (window.location.href.indexOf('http://www.mysgdev.com') === 0) {
+        window.location.href = 'http://www.mysgdev.com/';
+    }
+</script>
