8b02fb83d755b0433cd497c24e911a0c1ce3253d
Anonoz Chong <everything@anonoz.com>
2014-07-02 20:59:29 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,5 @@
 <script>
-    if (window.location.href.indexOf('http://www.mysgdev.com') === 0) {
+    if (window.location.href.indexOf('http://www.mysgdev.com') === -1) {
         window.location.href = 'http://www.mysgdev.com/';
     }
 </script>
